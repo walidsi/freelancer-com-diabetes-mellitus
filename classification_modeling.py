ba67726f056ff3c1fcf96cd877451c7ee61f760b
walidsi <42148514+walidsi@users.noreply.github.com>
2022-05-09 22:17:45 +0200

    Bug fix ... training dataset size for training accuracy prediction

--- classification_modeling.py
+++ classification_modeling.py
@@ -68,25 +68,28 @@ def train_predict(learner, sample_size, X_train, y_train, X_test, y_test):
     # TODO: Calculate the training time
     results['train_time'] = end - start
 
+    training_set_size_to_predict = len(y_test)  # or 300
+
     # TODO: Get the predictions on the test set(X_test),
     #       then get predictions on the first 300 training samples(X_train) using .predict()
     start = time()  # Get start time
     predictions_test = learner.predict(X_test)
-    predictions_train = learner.predict(X_train[:300])
+    predictions_train = learner.predict(X_train[:training_set_size_to_predict])
     end = time()  # Get end time
 
     # TODO: Calculate the total prediction time
     results['pred_time'] = end - start
 
     # TODO: Compute accuracy on the first 300 training samples which is y_train[:300]
-    results['acc_train'] = accuracy_score(y_train[:300], predictions_train)
+    results['acc_train'] = accuracy_score(
+        y_train[:training_set_size_to_predict], predictions_train)
 
     # TODO: Compute accuracy on test set using accuracy_score()
     results['acc_test'] = accuracy_score(y_test, predictions_test)
 
     # TODO: Compute F-score on the the first 300 training samples using fbeta_score()
     results['f_train'] = fbeta_score(
-        y_train[:300], predictions_train, beta=_g_beta)
+        y_train[:training_set_size_to_predict], predictions_train, beta=_g_beta)
 
     # TODO: Compute F-score on the test set which is y_test
     results['f_test'] = fbeta_score(y_test, predictions_test, beta=_g_beta)
@@ -177,7 +180,7 @@ def optimize_model(model: BaseEstimator, parameters, X_train: pd.DataFrame, y_tr
     print("Accuracy score on testing data: {:.4f}".format(
         accuracy_score(y_test, predictions)))
     print(
-        "F-score on testing data: {:.4f}".format(fbeta_score(y_test, predictions, beta=0.5)))
+        "F-score on testing data: {:.4f}".format(fbeta_score(y_test, predictions, beta=_g_beta)))
     print("\nOptimized Model\n------")
     print("Final accuracy score on the testing data: {:.4f}".format(
         accuracy_score(y_test, best_predictions)))
